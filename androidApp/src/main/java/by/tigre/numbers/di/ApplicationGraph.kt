1aeb155aa724c8b34f6362f1bd7ff090aca4dbc7
Tigre <k.gorbats@gmail.com>
2024-12-21 11:35:05 +0300

    Updated analytics settings

--- androidApp/src/main/java/by/tigre/numbers/di/ApplicationGraph.kt
+++ androidApp/src/main/java/by/tigre/numbers/di/ApplicationGraph.kt
@@ -2,6 +2,7 @@ package by.tigre.numbers.di
 
 import android.content.Context
 import by.tigre.numbers.analytics.Tracker
+import by.tigre.tools.tools.coroutines.CoreScope
 import by.tigre.tools.tools.coroutines.CoroutineModule
 
 class ApplicationGraph(
@@ -14,10 +15,10 @@ class ApplicationGraph(
     companion object {
         fun create(
             context: Context,
-            tracker: Tracker
+            tracker: (CoreScope) -> Tracker
         ): ApplicationGraph {
             val coroutineModule = CoroutineModule.Impl()
-            val analyticsModule = AnalyticsModule.Impl(tracker, coroutineModule)
+            val analyticsModule = AnalyticsModule.Impl(tracker(coroutineModule.scope), coroutineModule)
             val storeModule = StoreModule.Impl(context, coroutineModule = coroutineModule)
             return ApplicationGraph(storeModule, analyticsModule)
         }

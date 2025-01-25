9682696b1acad8169d486ad306fe82a489703f41
Tigre <k.gorbats@gmail.com>
2025-01-25 14:19:56 +0300

    Update app version

--- buildSrc/src/main/kotlin/Application.kt
+++ buildSrc/src/main/kotlin/Application.kt
@@ -1,6 +1,6 @@
 object Application {
     const val id: String = "by.tigre.numbers"
-    val version: Version = Version(0, 13, 1)
+    val version: Version = Version(0, 14, 0)
     const val name: String = "Numbers" // TODO move to xml
 
     const val SDK_COMPILE = 34

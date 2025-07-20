544e10c97726504e3e27755f8c4c8009d4c8811e
Tigre <k.gorbats@gmail.com>
2025-07-20 09:43:39 +0300

    Updated target sdk to 36

--- buildSrc/src/main/kotlin/Application.kt
+++ buildSrc/src/main/kotlin/Application.kt
@@ -3,9 +3,9 @@ object Application {
     val version: Version = Version(0, 17, 0)
     const val name: String = "Numbers" // TODO move to xml
 
-    const val SDK_COMPILE = 34
+    const val SDK_COMPILE = 36
     const val SDK_MINIMUM = 26
-    const val SDK_TARGET = 34
+    const val SDK_TARGET = 36
 
     data class Version(private val major: Int, private val minor: Int, private val patch: Int) {
         val code = 10000 * major + 100 * minor + patch
